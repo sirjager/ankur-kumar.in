f8279c19953fa4478bec59ca87f8b11368004f4b
Ankur Kumar <kankurcool@gmail.com>
2024-04-19 07:40:57 +0530

    chore: updated footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import {loadEnv} from "vite";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import robots from "astro-robots-txt";
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 import qwik from "@qwikdev/astro";
 import mdx from "@astrojs/mdx";
+import cloudflare from "@astrojs/cloudflare";
 
 import {toString} from "mdast-util-to-string";
 import readingTime from "reading-time";
@@ -55,9 +56,8 @@ export default defineConfig({
 		defaultStrategy: "viewport",
 	},
 	output: "hybrid",
-	adapter: node({
-		mode: "standalone",
-	}),
+	// adapter: node({mode: "standalone"}),
+	adapter: cloudflare({imageService: "cloudflare"}),
 	image: {
 		remotePatterns: [{protocol: "https"}, {protocol: "http"}],
 	},
