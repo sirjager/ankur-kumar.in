84ee9ea84455823c94e696d5e111802d2bd32d0b
Ankur Kumar <18713058+sirjager@users.noreply.github.com>
2024-05-13 21:52:03 +0530

    chore: updated env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,10 +4,6 @@
 declare module "astro-imagetools/components";
 
 interface ImportMetaEnv {
-	readonly APP_URL: string;
-	readonly APP_NAME: string;
-	readonly COUNTER_API_KEY: string;
-	readonly REDIS_CONNECTION_STRING: string;
 	readonly REDIS_REST_ENDPOINT: string;
 	readonly REDIS_ACCESS_TOKEN: string;
 }
